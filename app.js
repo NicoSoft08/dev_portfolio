69db11bb7cbaaf599019e06d6b9cc773ee0af416
NicoSoft08 <135339436+NicoSoft08@users.noreply.github.com>
2023-06-13 03:33:06 +0300

    app.js

--- app.js
+++ app.js
@@ -9,23 +9,34 @@ scrollUp.addEventListener("click", () => {
   });
 });
 
-// Nav hamburgerburger selections
+// Declaring and initializing variables
 
 const burger = document.querySelector("#burger_menu");
+const closer = document.querySelector("#closer_menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
+const navLink = document.querySelectorAll(".nav_link");
 
+// Opening 
 burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
+    burger.style.display = 'none';
+    closer.style.display = 'block';
+    ul.classList.add("show");
+});
+
+// Closing
+closer.addEventListener("click", () => {
+    burger.style.display = 'block';
+    closer.style.display = 'none';
+    ul.classList.remove("show");
 });
 
 // Close hamburger menu when a link is clicked
 
-// Select nav links
-const navLink = document.querySelectorAll(".nav_link");
-
 navLink.forEach((link) =>
   link.addEventListener("click", () => {
+    closer.style.display = 'none';
+    burger.style.display = 'block';
     ul.classList.remove("show");
   })
 );
